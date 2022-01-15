faf50a4622004484481438e370f7ec896e4ffb2f
Lauty <lopezlautarojoaquin1@gmail.com>
2022-01-15 01:24:29 -0300

    Configuraciones para deployment

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(cors())
 //Express.json
 app.use(express.json({ extended: true }))
 //Puerto
-const PORT = process.env.PORT || 4000
+const port = process.env.PORT || 4000
 //Rutas
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
 })
